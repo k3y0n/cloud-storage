0573d1a15715dc8e2a9581e48488de5a3332e055
k3y0n <y3.0n@yandex.by>
2024-05-08 13:38:14 +0300

    add swagger  && auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { FilesModule } from './files/files.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/entities/user.entity';
+import { File } from './files/entities/file.entity';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { AuthclearService } from './authclear/authclear.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT || 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: 'cloud-storage',
+      entities: [User, File],
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UsersModule,
+    FilesModule,
+    AuthModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AuthclearService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as express from 'express';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: false });
   app.enableCors();
   app.useGlobalPipes();
+
+  app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
+
+  const config = new DocumentBuilder()
+    .setTitle('Cloud storage example')
+    .setDescription('The cloud storage API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3001);
 }
 bootstrap();
